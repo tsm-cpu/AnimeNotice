a2704fe908557482a5ae1212d34c76c5d1a1e051
tsm-cpu <tsmyuma0609@gmail.com>
2026-06-20 21:28:15 +0900

    fix circular dependency between SecurityConfig and UserService

--- src/main/java/com/animenotice/config/SecurityConfig.java
+++ src/main/java/com/animenotice/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,11 +16,6 @@ public class SecurityConfig {
 
     @Autowired
     private UserService userService;
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
